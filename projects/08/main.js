38c4d8d5449e55536be672a5a6c6faff03a9bd9e
Kevin Win <mail@kevinwin.com>
2018-03-27 20:05:54 -0700

    Add main program for vm translator

--- projects/08/main.js
+++ projects/08/main.js
@@ -8,6 +8,24 @@ const isDirectory = fs.lstatSync(vmPath).isDirectory();
 const source = isDirectory ? fs.readdirSync(vmPath).filter(file => path.extname(file) === '.vm').map(file => vmPath + file) : [vmPath];
 const destination = isDirectory ? `${vmPath}/${path.basename(vmPath)}.asm`.replace('//', '/') :  vmPath.replace(/vm$/, 'asm')
 
+let hasSysInit = false;
+const asms = [];
+
+for (const vmPath of vmPaths) {
+    const prefix = path.basename(vmPath, '.vm');
+    const vmCode = fs.readFileSync(vmPath, {encoding: 'utf8'});
+
+    hasSysInit = hasSysInit || /^\s*function\s+Sys\.init\s+0\s*$/m.test(vmCode);
+
+    const asm = translate(prefix, vmCode);
+    asms.push(asm);
+}
+
+hasSysInit && asms.splice(0, 0, bootstrap());
+
+const asm = asms.join('\n');
+
+fs.writeFileSync(destination, asm, {encoding: 'utf8'});
 
 
 
