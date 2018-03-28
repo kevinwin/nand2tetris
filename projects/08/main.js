7cd958ce00c969e88a94efe68f6279eed33b03f7
Kevin Win <mail@kevinwin.com>
2018-03-28 16:14:02 -0700

    Check that path is directory before appending missing '/'

--- projects/08/main.js
+++ projects/08/main.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const {bootstrap, convert} = require('./vm');
 
-const vmPath = /\/$/.test(process.argv[2]) ? process.argv[2] : process.argv[2] + '/';
+let vmPath = process.argv[2];
 const isDirectory = fs.lstatSync(vmPath).isDirectory();
+vmPath = isDirectory && !(/\/$/.test(vmPath)) ? `${vmPath}/` : vmPath
 
 const source = isDirectory ? fs.readdirSync(vmPath).filter(file => path.extname(file) === '.vm').map(file => vmPath + file) : [vmPath];
 const destination = isDirectory ? `${vmPath}/${path.basename(vmPath)}.asm`.replace('//', '/') :  vmPath.replace(/vm$/, 'asm')
