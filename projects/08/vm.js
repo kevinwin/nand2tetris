0ff17221ffb7b00c02efcc58e10337244882b75e
Kevin Win <mail@kevinwin.com>
2018-03-27 20:15:27 -0700

    Add vm orchestrator

---
+++ projects/08/vm.js
@@ -0,0 +1,24 @@
+const parse = require('.parser');
+const translate = require('./translator');
+
+function bootstrap() {
+    return [...translate(null, {type: 'bootstrap'})].join('\n');
+}
+
+function convert(prefix, vmCode) {
+    const lines = vmCode.split(/(\r|\n)/);
+    const translatedLines = [];
+
+    for (const line of lines) {
+        const command = parse(line);
+        translatedLines.push(...translate(prefix, command));
+    }
+
+    return translatedLines.join('\n');
+}
+
+module.exports = {
+    bootstrap,
+    convert
+}
+
